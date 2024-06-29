8c9e77e661c0cc39630ad2b099d7f9d0b90e44a0
Outdatedcandy92 <138517406+Outdatedcandy92@users.noreply.github.com>
2024-06-28 22:39:48 -0400

    settings page

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 import re  
 import os
-from flask import Flask, render_template
+from flask import Flask, request, redirect, url_for, render_template
+
 from dotenv import load_dotenv
 
 app = Flask(__name__)
 
 url = os.getenv('URL')
+
+Reward_Name = 'Set Name'
+Ticket_Number = 'set number'
+Image_Url = 'set url'
 
 def get_data():
     response = requests.get(url)
@@ -26,13 +31,23 @@ def get_data():
 
 @app.route('/')
 def home():
-    number = get_data()
+    number = get_data()  # Assuming get_data() is defined elsewhere
     remaining = 70 - int(number)
     percent = int(number) / 70 * 100
 
-    return render_template('index.html', number=number, remaining=remaining,percent=percent)
+    return render_template('index.html', number=number, remaining=remaining, percent=percent)
 
+@app.route('/set', methods=['POST'])
+def set():
+    # Example of processing form data
+    shop_url = request.form['shopurl']
+    reward_name = request.form['rewardname']
+    ticket_number = request.form['rewardticket']
+    image_url = request.form['rewardimage']
+    # Process the data as needed...
 
+    # Redirect back to the home page
+    return redirect(url_for('home'))
 
 
 app.run(host='0.0.0.0', port=8080)
